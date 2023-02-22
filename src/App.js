6c90eae9a201b9a68f2654beb4d155660feb6cd0
Muvindu Rangana <72459961+muvindurangana@users.noreply.github.com>
2023-02-22 18:01:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+const courseContent =[{
+  status:'completed',
+  text :'git hub'
+},
+{
+  status:'completed',
+  text :'key frame'
+},
+{
+  status:'completed',
+  text :'ex 6'
+},
+{
+  status:'completed',
+  text :'smell react'
+},
+{
+  status:'completed',
+  text :'webpack'
+},
+{
+  status:'on-going',
+  text :'key frame'
+},{
+  status:'not started',
+  text :'file system'
+}
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='app'>
+    {courseContent.map((val,key)=>{
+      return <MyApp key={key} status={val.status} text={val.text} />
+    })}
+      
     </div>
   );
 }
 
 export default App;
+   
+function MyApp({status,text}){
+  
+  return <p style={{fontSize:'20px', color:status==='completed'?'green':status==='on-going'?'red':'yellow'}}>{text}</p>
+}
